9f9634692448173a68aed5d28eff8316cb31ed60
Lucas Pedroso <lucas.pedroso@dextra-sw.com>
2021-02-09 20:23:14 -0300

    feat: firebase config

---
+++ src/admin/firebase_config/config.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyAzwse_i7C-xDAAxRkRamEBPwPGgnM4nQE",
+    authDomain: "teste-cms-ea0ff.firebaseapp.com",
+    databaseURL: "https://teste-cms-ea0ff.firebaseio.com",
+    projectId: "teste-cms-ea0ff",
+    storageBucket: "teste-cms-ea0ff.appspot.com",
+    messagingSenderId: "751573960050",
+    appId: "1:751573960050:web:db59db09dc34c757bac9eb",
+    measurementId: "G-P8MX2M0JPL"
+}
+
+export default config
